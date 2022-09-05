4028686ef736feb4439acf76599c7d0c071530a8
Pablo Lichtig <plichtig@cheyenne5.cheyenne.ucar.edu>
2022-09-05 03:59:41 -0600

    add some if __name__ == __main__:

--- camchem/regridding_anthro_fv09_sectors_nowrap.py
+++ camchem/regridding_anthro_fv09_sectors_nowrap.py
@@ -39,83 +39,89 @@ def dst_file_name(output_path, orig_file, dst_res):
     # return orig_file.replace(orig_res, dst_res)
     return output_path + dst_res +'_' + orig_file
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# ! Prepare Grid and compute weights !
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Load
-list_of_emis = [input_path + item for item in pattern_of_filenames]
 
-print('Using files:')
-print(*list_of_emis, sep='\n')
-
-#Create weights
-#If there is a previous one, delete it
-weights = f'{output_grid[:-3]}_WEIGHTS_sectors.nc'
-try:
-    os.remove(weights)
-    print('removing wgt_file')
-except OSError:
-    pass
-
-
-print('Creating weights')
-emis_for_wgt = list_of_emis[0]
-with xr.open_dataset(emis_for_wgt) as emis:
-    vvld.Regridding(
-                    var_array=emis[fields[0]],
-                    src_grid_file=input_grid,
-                    dst_grid_file=output_grid, creation_date=False,
-                    wgt_file=weights, save_wgt_file=True, 
-                    save_wgt_file_only=True, dst_file=weights)
-print(f'Weights file = {weights}')
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !       Regrid and add date        !
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-for emis_name in list_of_emis:
-    print("################################################################\n",
-          emis_name,
-          "\n################################################################")
+def main():
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # ! Prepare Grid and compute weights !
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Load
+    list_of_emis = [input_path + item for item in pattern_of_filenames]
     
-    with xr.open_dataset(emis_name) as emis:
-        # The split is to select emis_name after last '/' char, so that
-        # the emis_path is not copied
-        dst_file = dst_file_name(output_path, dst_res, emis_name.split('/')[-1])
-        print(f'dst_file = {dst_file}')
-# Uncomment for dinamically changing fields
-#########
-        fields = []
-        for var in emis.variables:
-            if len(emis[var].dims) >+ 2:
-                fields.append(var)
-#########
-        print(f'regridding fields: {fields}')
+    print('Using files:')
+    print(*list_of_emis, sep='\n')
+    
+    #Create weights
+    #If there is a previous one, delete it
+    weights = f'{output_grid[:-3]}_WEIGHTS_sectors.nc'
+    try:
+        os.remove(weights)
+        print('removing wgt_file')
+    except OSError:
+        pass
+    
+    
+    print('Creating weights')
+    emis_for_wgt = list_of_emis[0]
+    with xr.open_dataset(emis_for_wgt) as emis:
         vvld.Regridding(
-                        var_array=emis,
-                        fields=fields,
-                   #    add_fields=['date'],
+                        var_array=emis[fields[0]],
                         src_grid_file=input_grid,
-                        dst_grid_file=output_grid,
-                        wgt_file=weights,
-                        save_wgt_file=False,
-                        save_results=True,
-                        creation_date=False,
-                        dst_file=dst_file)
-    gc.collect()
+                        dst_grid_file=output_grid, creation_date=False,
+                        wgt_file=weights, save_wgt_file=True, 
+                        save_wgt_file_only=True, dst_file=weights)
+    print(f'Weights file = {weights}')
 
- 
-# Add date (Doseong's vivaldi_a code doesn't do it)
-# emis_regridded = sorted(glob.glob(f'{output_path}*{dst_res}'))
-# print('Adding dates')
-# for i in emis_regridded:
-#     print(f'i:')
-#     with xr.open_dataset(i, mode='a') as ds:
-#         date = ds['time'].dt.strftime('%Y%m%d').astype(int)
-#         ds = ds.assign(date=date)
-#         ds['date'] = ds['date'].assign_attrs(units="YYYYMMDDY",
-#                                              long_name="Date")
-#         ds['time'].encoding['units'] = 'hours since 1850-01-01'
-#         ds.to_netcdf(i, format='NETCDF3_64BIT')
-#     print(f'{i} done')
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # !       Regrid and add date        !
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    
+    for emis_name in list_of_emis:
+        print("################################################################\n",
+              emis_name,
+              "\n################################################################")
+        
+        with xr.open_dataset(emis_name) as emis:
+            # The split is to select emis_name after last '/' char, so that
+            # the emis_path is not copied
+            dst_file = dst_file_name(output_path, dst_res, emis_name.split('/')[-1])
+            print(f'dst_file = {dst_file}')
+    # Uncomment for dinamically changing fields
+    #########
+            fields = []
+            for var in emis.variables:
+                if len(emis[var].dims) >+ 2:
+                    fields.append(var)
+    #########
+            print(f'regridding fields: {fields}')
+            vvld.Regridding(
+                            var_array=emis,
+                            fields=fields,
+                       #    add_fields=['date'],
+                            src_grid_file=input_grid,
+                            dst_grid_file=output_grid,
+                            wgt_file=weights,
+                            save_wgt_file=False,
+                            save_results=True,
+                            creation_date=False,
+                            dst_file=dst_file)
+        gc.collect()
+    
+     
+    # Add date (Doseong's vivaldi_a code doesn't do it)
+    # emis_regridded = sorted(glob.glob(f'{output_path}*{dst_res}'))
+    # print('Adding dates')
+    # for i in emis_regridded:
+    #     print(f'i:')
+    #     with xr.open_dataset(i, mode='a') as ds:
+    #         date = ds['time'].dt.strftime('%Y%m%d').astype(int)
+    #         ds = ds.assign(date=date)
+    #         ds['date'] = ds['date'].assign_attrs(units="YYYYMMDDY",
+    #                                              long_name="Date")
+    #         ds['time'].encoding['units'] = 'hours since 1850-01-01'
+    #         ds.to_netcdf(i, format='NETCDF3_64BIT')
+    #     print(f'{i} done')
+
+# =================================================
+if __name__ == '__main__':
+    main()
